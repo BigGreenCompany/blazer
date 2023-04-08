190f74cca8a46f59bdd8c6371c846d1b5862c15f
Ian Heraty <ian.heraty@pm.me>
2023-04-07 23:43:12 -0500

    Add messages renderer to fix code block styling issue.

--- app/helpers/blazer/messages_helper.rb
+++ app/helpers/blazer/messages_helper.rb
@@ -3,6 +3,30 @@ require "redcarpet"
 module Blazer::MessagesHelper
 
   def markdown
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    renderer = Redcarpet::Render::HTML.new(
+      # Specify the CSS classes to be applied to code blocks
+      :with_toc_data => true,
+      :hard_wrap => true,
+      :fenced_code_blocks => true,
+      :no_intra_emphasis => true,
+      :highlight => true,
+      :quote => true,
+      :footnotes => true,
+      :strikethrough => true,
+      :underline => true,
+      :highlight => true,
+      :tables => true,
+      :space_after_headers => true,
+      :autolink => true,
+      :disable_indented_code_blocks => true,
+      :prettify => true,
+      :superscript => true,
+      :link_attributes => {rel: 'nofollow', target: "_blank"},
+      # Specify the CSS classes to be applied to code blocks
+      :code_attr => 'class="my-code-block-class"',
+      :codespan_attr => 'class="my-code-span-class"'
+    )
+
+    Redcarpet::Markdown.new(renderer)
   end
 end
